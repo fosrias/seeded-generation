51f7cd6f13de93937fbb86560293f7c3a94e6871
mfoster <fosterinfo@gmail.com>
2009-11-13 13:52:53 -0800

    Updated override_rake_task for task dependencies.

--- lib/override_rake_task.rb
+++ lib/override_rake_task.rb
@@ -1,21 +1,19 @@
 # OverrideRakeTask
 require 'rake'
 
-Rake::TaskManager.class_eval do  
+Rake::TaskManager.class_eval do
   def alias_task(old_name, new_name)
     @tasks[new_name.to_s] = @tasks.delete(old_name.to_s)
   end 
 end
-
+ 
 def alias_task(old_name, new_name)
   Rake.application.alias_task(old_name, new_name)
 end
 
-# Overrides a task and creates an aliased task 'old_name:original'
-# that can be called in the overriding task block
-def override_task(args, &block)
-  name, deps = Rake.application.resolve_args([args])  
+def override_task(*args, &block)
+  name, params, deps = Rake.application.resolve_args(args)
   old_name = Rake.application[name].name
   alias_task old_name, old_name + ":original"
-  task(args, &block)
+  Rake::Task.define_task({name => deps}, &block)
 end
